dc340893503cdd35c5c089668ac14460b988303c
sweetcapucino <thuylinh271055@gmail.com>
2022-04-01 14:02:43 +0700

    init

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./App.css";
+import "./main.css";
+
+function App() {
+    return <div className="App bg-white text-green-500">hi</div>;
+}
+
+export default App;
