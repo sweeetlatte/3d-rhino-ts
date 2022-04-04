d9e206a47bef59dae3d812120137549eec6d772f
sweetcapucino <thuylinh271055@gmail.com>
2022-04-04 14:22:37 +0700

    M: eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
   extends: [
     'plugin:react/recommended',
     'airbnb',
-    'airbnb-typescript',
+    'airbnb-typescript/base',
     'plugin:@typescript-eslint/recommended'
   ],
   parser: '@typescript-eslint/parser',
