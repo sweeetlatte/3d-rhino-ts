90aa6adafbb4b82270776b85e00ad52137cee0c9
sweetcapucino <thuylinh271055@gmail.com>
2022-04-04 10:43:50 +0700

    setup eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import { Physics } from "@react-three/cannon";
